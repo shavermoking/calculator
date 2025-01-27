5fa368d656c89a87d99702a7795d02ce71c06619
Nikita Churkin <n.churkin@bnovo.ru>
2025-01-28 00:52:56 +0300

    finished calculator

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	for {
+		var first float64
+		var second float64
+		var char string
+
+		fmt.Print("Введите первое число: ")
+		_, err := fmt.Scan(&first)
+		if err != nil {
+			fmt.Println("Ошибка ввода первого числа")
+			return
+		}
+
+		fmt.Print("Введите второе число: ")
+		_, err = fmt.Scan(&second)
+		if err != nil {
+			fmt.Println("Ошибка ввода второго числа")
+			return
+		}
+
+		fmt.Print("Введите знак (+, -, *, /, ^) ")
+		_, err = fmt.Scan(&char)
+		if err != nil {
+			fmt.Println("Ошибка ввода знака")
+			fmt.Println(err)
+			return
+		}
+
+		switch char {
+		case "+":
+			fmt.Printf("Результат: %.2f\n", first+second)
+		case "-":
+			fmt.Printf("Результат: %.2f\n", first-second)
+		case "*":
+			fmt.Printf("Результат: %.2f\n", first*second)
+		case "/":
+			if second == 0 {
+				fmt.Println("Ошибка: деление на ноль") // Исправлено: добавлен \n
+			} else {
+				fmt.Printf("Результат: %.2f\n", first/second)
+			}
+		case "^":
+			fmt.Printf("Результат: %.2f\n", math.Pow(first, second))
+		default:
+			fmt.Println("Неизвестная операция")
+		}
+
+		fmt.Scanln()
+
+		fmt.Print("Хотите продолжить? (y/n): ")
+		var choice string
+		_, err = fmt.Scan(&choice)
+		if err != nil {
+			fmt.Println("Ошибка ввода выбора")
+		}
+
+		if choice != "y" {
+			fmt.Println("Завершение программы")
+			break
+		}
+	}
+}
